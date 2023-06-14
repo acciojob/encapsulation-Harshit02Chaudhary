73cebfb99a92605623b12cd7a3bc1bdd2226fdd4
Harshit02Chaudhary <chaudhary2sep@gmail.com>
2023-06-14 22:47:34 +0530

    Added Solution

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,16 @@
 package com.driver;
 
 public class Main {
-  
+
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly();
+        //obj.name = "Harshit";
+        //assigning error - Unexpected token
+        //System.out.println(obj.name);
+        //printing error - name has private access in "com.driver.RWOnly"
+
+        obj.setName("Harshit");
+        System.out.println(obj.getName());
+    }
+
 }
